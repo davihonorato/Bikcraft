4fe8a03c8eb8321f5955f90144c5162923957ca3
Davi Honorato <davihonoratoctt@gmail.com>
2022-12-31 08:50:01 -0300

    consertando erro crítico no primeiro include da página portfolio

--- page-portfolio.php
+++ page-portfolio.php
@@ -5,7 +5,7 @@ get_header();
 ?>
 
 <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-	<?php include(TEMPLATEPATH . "/inc/introducao.php"); ?>
+	<?php include(TEMPLATEPATH . "/inc/section-introducao.php"); ?>
 
 	<section class="container animar-interno">
 		<ul class="rslides">
