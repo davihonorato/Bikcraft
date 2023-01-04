388c1992bc9386cb3f98b035ef98f6acca288113
Davi Honorato <davihonoratoctt@gmail.com>
2023-01-04 20:20:20 -0300

    Carregando, dinamicamente, o conteúdo da home.

--- page-inicio.php
+++ page-inicio.php
@@ -7,12 +7,12 @@ get_header();
 <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 		<section class="introducao">
 			<div class="container">
-				<h1>Bicicletas Feitas a Mão</h1>
+				<h1><?php the_field('titulo_da_introducao'); ?></h1>
 				<blockquote class="quote-externo">
-					<p>“não tenha nada em sua casa que você não considere útil ou acredita ser bonito”</p>
-					<cite>WILLIAM MORRIS</cite>
+					<p><?php the_field('citacao_da_introducao'); ?></p>
+					<cite><?php the_field('autor_da_citacao'); ?></cite>
 				</blockquote>
-				<a href="produtos.html" class="btn">Orçamento</a>
+				<a href="/produtos" class="btn"><?php the_field('texto_do_botao_da_introducao'); ?></a>
 			</div>
 		</section>
 		
@@ -47,8 +47,8 @@ get_header();
 			</ul>
 
 			<div class="call">
-				<p>clique aqui e veja os detalhes dos produtos</p>
-				<a href="/produtos" class="btn btn-preto">Produtos</a>
+				<p><?php the_field('chamada_para_produtos'); ?></p>
+				<a href="/produtos" class="btn btn-preto"><?php the_field('texto_do_botao'); ?></a>
 			</div>
 
 		</section>
