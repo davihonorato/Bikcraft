0799bdde60cca06feab2f7035e34e547b2b63854
Davi Honorato <davihonoratoctt@gmail.com>
2023-01-05 08:05:46 -0300

    Criando funções que removem códigos adicionados automaticamente pelo WP, mas que não serão utilizados em nosso site.

--- functions.php
+++ functions.php
@@ -1,11 +1,24 @@
 <?php 
 
-add_action('init', 'remove_content_editor');
-/**
- * Remove the content editor from ALL pages 
+// Funções para Limpar o Header
+remove_action('wp_head', 'rsd_link');
+remove_action('wp_head', 'wlwmanifest_link');
+remove_action('wp_head', 'start_post_rel_link', 10, 0 );
+remove_action('wp_head', 'adjacent_posts_rel_link_wp_head', 10, 0);
+remove_action('wp_head', 'feed_links_extra', 3);
+remove_action('wp_head', 'wp_generator');
+remove_action('wp_head', 'print_emoji_detection_script', 7);
+remove_action('admin_print_scripts', 'print_emoji_detection_script');
+remove_action('wp_print_styles', 'print_emoji_styles');
+remove_action('admin_print_styles', 'print_emoji_styles');
+
+
+/*
+ * Função para remover editor de conteúdo em TODAS as páginas do WP
  */
 function remove_content_editor() { 
     remove_post_type_support('page', 'editor');       
 }
+add_action('init', 'remove_content_editor');
 
 ?>
