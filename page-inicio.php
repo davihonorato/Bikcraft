ed5eb38ed8b2b3d2dafead0ba73f23a26a28f15d
Davi Honorato <davihonoratoctt@gmail.com>
2023-01-05 09:05:46 -0300

    Tornando o background da home dinamico e responsivo. Bug: a imagem não está aparecendo.

--- page-inicio.php
+++ page-inicio.php
@@ -5,6 +5,31 @@ get_header();
 ?>
 
 <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+
+<?php 
+	$background_id = get_field('background_home');
+
+	$background_home_large = wp_get_attachment_image_src($background_id, 'large');
+	$background_home_medium = wp_get_attachment_image_src($background_id, 'medium');
+?>
+
+<style type="text/css">
+	.introducao {
+		background: url("<?php $background_home_large[0] ?>") no-repeat center center;
+		background-size: cover;
+	}
+	@media only screen and (max-width: 767px) {
+		.introducao {
+			background: url("<?php $background_home_medium[0] ?>") no-repeat center center;
+			background-size: cover;
+		}
+	}
+</style>
+
+<script>
+	console.log('valor: <?php $background_id ?>')
+</script>
+
 		<section class="introducao">
 			<div class="container">
 				<h1><?php the_field('titulo_da_introducao'); ?></h1>
@@ -15,7 +40,7 @@ get_header();
 				<a href="/produtos" class="btn"><?php the_field('texto_do_botao_da_introducao'); ?></a>
 			</div>
 		</section>
-		
+
 		<section class="produtos container animar">
 			<h2 class="subtitulo">Produtos</h2>
 			<ul class="produtos_lista">
