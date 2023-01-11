2e607809baeedc46b2ddb3747c8a5025940539c7
Davi Honorato <davihonoratoctt@gmail.com>
2023-01-10 22:52:22 -0300

    Corrigindo erro crítico no footer em outras páginas além da página 'contato'.

--- inc/redes-sociais.php
+++ inc/redes-sociais.php
@@ -1,14 +1,16 @@
+<?php $contato = get_page_by_title('contato') ?>
+
 <ul>
 
-    <?php if(have_rows('redes_sociais')):
+    <?php if(have_rows('redes_sociais', $contato)):
 
-        while(have_rows('redes_sociais')): 
+        while(have_rows('redes_sociais', $contato)): 
 
             the_row(); ?>
 
                 <li>
-                    <a href="<?php the_sub_field('url_rede_social') ?>" target="_blank">
-                        <img src="<?php the_sub_field('icone_rede_social') ?>" alt="<?php the_sub_field('nome_da_rede_social') ?> Bikcraft">
+                    <a href="<?php the_sub_field('url_rede_social', $contato) ?>" target="_blank">
+                        <img src="<?php the_sub_field('icone_rede_social', $contato) ?>" alt="<?php the_sub_field('nome_da_rede_social') ?> Bikcraft">
                     </a>
                 </li>
 
