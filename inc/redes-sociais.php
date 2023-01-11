2ebd1093090af9e614dee042b7a4811877c0341f
Davi Honorato <davihonoratoctt@gmail.com>
2023-01-10 23:29:42 -0300

    Carregando, dinamicamente, o conteúdo do footer.

--- inc/redes-sociais.php
+++ inc/redes-sociais.php
@@ -2,20 +2,20 @@
 
 <ul>
 
-    <?php if(have_rows('redes_sociais', $contato)):
+<?php if(have_rows('redes_sociais', $contato)):
 
-        while(have_rows('redes_sociais', $contato)): 
+    while(have_rows('redes_sociais', $contato)): 
 
-            the_row(); ?>
+        the_row(); ?>
 
-                <li>
-                    <a href="<?php the_sub_field('url_rede_social', $contato) ?>" target="_blank">
-                        <img src="<?php the_sub_field('icone_rede_social', $contato) ?>" alt="<?php the_sub_field('nome_da_rede_social') ?> Bikcraft">
-                    </a>
-                </li>
+            <li>
+                <a href="<?php the_sub_field('url_rede_social', $contato) ?>" target="_blank">
+                    <img src="<?php the_sub_field('icone_rede_social', $contato) ?>" alt="<?php the_sub_field('nome_da_rede_social') ?> Bikcraft">
+                </a>
+            </li>
 
-        <?php endwhile;
-        
-    endif; ?>
+    <?php endwhile;
+    
+endif; ?>
 
 </ul>
